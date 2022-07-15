61382ee8c663757d7241d014725ee6423a0b3087
Felipe Azzolino Varella <felipe_azzolinovarella@hotmail.com>
2022-07-15 19:11:45 -0300

    modificação teste

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.MINTY])
 
-app.layout = layout
+app.layout = layout  # Alfa
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
